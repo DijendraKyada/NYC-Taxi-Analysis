46ac43ffecf1d601f624f7c63580f51f24f0c0bc
Dijendra <kyadad@clarkson.edu>
2020-03-05 12:10:18 -0500

    Checking

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import csv,time
+import csv
+import time
+
+start = time.time()
+
+fn = 'Sources/trip_data_7.csv'
+f = open(fn, "r")
+n = 0
+for line in f:
+    print(line)
+    break
+print(time.time()-start)
+print("Hello")
