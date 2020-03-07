80453993b206bcbfa86d2292ba4ec28ac1a50bff
Dijendra <kyadad@clarkson.edu>
2020-03-07 16:47:45 -0500

    Done

--- main.py
+++ main.py
@@ -14,11 +14,14 @@ n = 0
 number_of_rows = 0
 min_pickup_time = None
 max_dropoff_time = None
-pickup_lon = {}
-pickup_lat = {}
-dropoff_lon = {}
-dropoff_lat = {}
-
+pickup_min_lon = None
+pickup_min_lat = None
+dropoff_min_lon = None
+dropoff_min_lat = None
+pickup_max_lon = None
+pickup_max_lat = None
+dropoff_max_lon = None
+dropoff_max_lat = None
 
 for row in reader:
     # printing header
@@ -49,11 +52,37 @@ for row in reader:
         elif dropofftime > max_dropoff_time:
             max_dropoff_time = dropofftime
 
-        # pickup Longitude
-        if row[10] in pickup_lon:
-            pickup_lon[row[10]] += 1
-        else:
-            pickup_lon[row[10]] = 1
+        # min pickup
+        if pickup_min_lon is None and pickup_min_lat is None:
+            pickup_min_lon = row[10]
+            pickup_min_lat = row[11]
+        elif pickup_min_lon > row[10] and pickup_min_lat > row[11]:
+            pickup_min_lon = row[10]
+            pickup_min_lat = row[11]
+
+        # max pickup
+        if pickup_max_lon is None and pickup_max_lat is None:
+            pickup_max_lon = row[10]
+            pickup_max_lat = row[11]
+        elif pickup_max_lon < row[10] and pickup_max_lat < row[11]:
+            pickup_max_lon = row[10]
+            pickup_max_lat = row[11]
+
+        # min dropoff
+        if dropoff_min_lon is None and dropoff_min_lat is None:
+            dropoff_min_lon = row[12]
+            dropoff_min_lat = row[13]
+        elif dropoff_min_lon > row[12] and dropoff_min_lat > row[13]:
+            dropoff_min_lon = row[12]
+            dropoff_min_lat = row[13]
+
+        # max dropoff
+        if dropoff_max_lon is None and dropoff_max_lat is None:
+            dropoff_max_lon = row[12]
+            dropoff_max_lat = row[13]
+        elif dropoff_max_lon < row[12] and dropoff_max_lat < row[13]:
+            dropoff_max_lon = row[12]
+            dropoff_max_lat = row[13]
 
     # incrementing pointer
     n += 1
@@ -69,8 +98,21 @@ print("Data ranges from:", min_pickup_time)
 # max_dropoff_time
 print("Data ranges till:", max_dropoff_time)
 
-# Pickup Longitude
-print("Pickup Longitude:", pickup_lon)
+# Min Pickup
+print("Min Pickup Longitude:", pickup_min_lon)
+print("Min Pickup Latitude:", pickup_min_lat)
+
+# Max Pickup
+print("Max Pickup Longitude:", pickup_max_lon)
+print("Max Pickup Latitude:", pickup_max_lat)
+
+# Min Pickup
+print("Min Dropoff Longitude:", dropoff_min_lon)
+print("Min Dropoff Latitude:", dropoff_min_lat)
+
+# Max Pickup
+print("Max Dropoff Longitude:", dropoff_max_lon)
+print("Max Dropoff Latitude:", dropoff_max_lat)
 
 # printing total time taken to run the script
 print(time.time()-start)
